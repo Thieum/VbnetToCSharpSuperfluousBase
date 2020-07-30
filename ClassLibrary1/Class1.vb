24b74eb37588d6579224b5d535579634948a37e5
Matthieu Penant <thieum22@hotmail.com>
2020-07-30 08:12:14 -0400

    Complete example for https://github.com/icsharpcode/CodeConverter/issues/600

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,17 @@
+﻿Public Class A
+
+    Protected Friend Overridable Sub Test()
+        Console.WriteLine("From A")
+    End Sub
+
+End Class
+
+Public Class B
+    Inherits A
+
+    Public Sub TestCaller()
+        Console.WriteLine("From B")
+        Test()
+    End Sub
+
+End Class
---
+++ ConsoleApp5/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports ClassLibrary1
+
+Module Module1
+
+    Public Class C
+        Inherits B
+
+        Protected Overrides Sub Test()
+            Console.WriteLine("From C")
+        End Sub
+
+    End Class
+
+
+    Sub Main()
+        Dim c As New C
+        c.TestCaller()
+        Console.ReadLine()
+    End Sub
+
+End Module
